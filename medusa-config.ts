de902ef705dad4547cf564d179f1cf512b41b047
kims07231992 <kims07231992@gmail.com>
2025-08-05 23:52:18 -0700

    Add Stripe module

--- medusa-config.ts
+++ medusa-config.ts
@@ -54,6 +54,20 @@ module.exports = defineConfig({
           },
         ],
       },
+    },
+    {
+      resolve: "@medusajs/medusa/payment",
+      options: {
+        providers: [
+          {
+            resolve: "@medusajs/medusa/payment-stripe",
+            id: "stripe",
+            options: {
+              apiKey: process.env.STRIPE_API_KEY,
+            },
+          },
+        ],
+      },
     }
   ]
 })
